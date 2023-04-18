2d741fed5e9a02e0339298428fef95559a46b551
Jacob Ormerod <jto888@users.noreply.github.com>
2023-04-18 10:19:26 -0400

    restore basic dem functionality

--- src/DEMwDep.cpp
+++ src/DEMwDep.cpp
@@ -20,15 +20,66 @@
 			
 			
 	std::unique_ptr<ElementList> EL(new ElementList(model_in, years_in));		
-			
-			
+	// EQ is a shared_ptr for storage in History permitting test output of the queue
+	std::shared_ptr<EventQueue> EQ(new EventQueue(EL, years_in));			
 	// note OLL is now a shared_ptr for storage in History		
-	std::shared_ptr<OpLineList> OLL(new OpLineList(EL, dep_in));		
+	std::shared_ptr<OpLineList> OLL(new OpLineList(EL, dep_in));
+	std::unique_ptr<History> H(new History(OLL, EQ, margin_in, show_zeros));
+//	int Elem0ID = EL->getByIndex(0)->numRands();
+//	Rcout<< "Elem0 numRands  "<< Elem0ID << std::endl;
+ // Rcout << EQ->writeQue();	
+//Rcout << EQ->getEvent(0)->getTime() << "  " << EQ->getEvent(1)->getTime() << "  " << EQ->getEvent(2)->getTime() << std::endl;	
+	bool endSimulation = false;
+	int headspace = 1;
+// This is the main loop for the simulation eventually will be a while loop
+//for(int i=0; i<40; i++) {	
+while(!endSimulation) {
+	
+	std::shared_ptr<DiscreteEvent> ev = EQ->pullNext();
+// before moving the pos index, set the last duration	
+	H->setLastDuration(ev->getTime());
+	
+	
+	
+	switch(ev->getType()) {			
+		case END_SIM:		
+			endSimulation = true;	
+			break;	
+		case FAILURE:		
+			OnFailure(ev, EL, EQ, OLL);	
+			break;	
+		case REPAIR:		
+			OnRepair(ev, EL, EQ, OLL);	
+			break;	
+				
+		default:		
+			endSimulation = true;	
+			break;	
+	}			
+
+
+	
+
+	headspace = H->update(ev);
+	if(headspace==0)  {
+		endSimulation = true;
+	}
 		
-	//return Rcpp::wrap(OLL->getByNum(9)->getDirectDependents());
-	//return Rcpp::wrap(OLL->getByNum(1)->getDirectDependents().empty());
-	//return Rcpp::wrap(OLL->getByNum(1)->getDirectDependents().size());		
-	//return Rcpp::wrap(OLL->getByNum(1)->getCoEnabledDependents().size());	
-	return Rcpp::wrap(OLL->getByNum(1)->getCoEnabledDependents());
-}			
-			
+}	
+
+	
+	Rcpp::List outframe=H->historyList();
+	//add headspace as an attribute to output
+	outframe.attr("headspace") = headspace;
+	Rcout<< "headspace    "<< headspace << std::endl;	
+	std::unique_ptr<Summary> S(new Summary(OLL));
+	outlist.push_back(S->summaryDF());
+	outlist.push_back(outframe);
+	outlist.push_back(Rcpp::wrap(H->get__detail()));
+
+
+	 return(outlist);
+
+
+
+}
